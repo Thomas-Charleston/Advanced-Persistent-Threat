759bfb2806b3e0b916ce05deab699a03a6f155aa
Thomas <thomas-charleston@outlook.com>
2025-12-20 12:10:00 +0000

    OS header UI
    Live date display + icons for home screen header

---
+++ Assets/Scripts/Date.cs
@@ -0,0 +1,56 @@
+using System.Net.Http.Headers;
+using UnityEngine;
+using TMPro;
+
+public class Date : MonoBehaviour
+{
+    public TMP_Text timeDisplay;
+    void Start()
+    {
+        string month = System.DateTime.UtcNow.ToLocalTime().ToString("MM");
+        switch (month)
+        {
+            case "01":
+                month = "Jan";
+                break;
+            case "02":
+                month = "Feb";
+                break;
+            case "03":
+                month = "Mar";
+                break;
+            case "04":
+                month = "Apr";
+                break;
+            case "05":
+                month = "May";
+                break;
+            case "06":
+                month = "Jun";
+                break;
+            case "07":
+                month = "Jul";
+                break;
+            case "08":
+                month = "Aug";
+                break;
+            case "09":
+                month = "Sep";
+                break;
+            case "10":
+                month = "Oct";
+                break;
+            case "11":
+                month = "Nov";
+                break;
+            case "12":
+                month = "Dec";
+                break;
+            default:
+                month = "ERR";
+                break;
+        }
+        string time = System.DateTime.UtcNow.ToLocalTime().ToString("dd " + month + " HH:mm");
+        timeDisplay.text= time;
+    }
+}
